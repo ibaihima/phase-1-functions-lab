ea817a692c43e0bb441457ccbcfbfd6900398644
Ibaihima <ibaihima@hotmail.com>
2022-01-31 17:18:21 -0500

    commit tree 9d5df157a1fddfa25124c6684ea65ba30514d85b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    const block = 42
+    if (someValue > block){
+        return someValue - block;
+    }
+    else 
+        return block - someValue
+}
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    const distance = (start - destination) 
+    return Math.abs (distance * 264)
+}
+function calculatesFarePrice(start, destination){
+    const dist = distanceTravelledInFeet(start, destination)
+    if (dist <= 400){
+    return 0;    
+    }
+    else if (dist > 400 && dist <= 2000){
+    return (dist - 400) *0.02
+    }
+    else if (dist > 2000 && dist <= 2500){
+    return 25
+    }
+    else 
+    return "cannot travel that far"
+}   
+
